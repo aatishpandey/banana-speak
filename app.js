5ca5c0f403a839a08cbc4397bdcd1a48be2695a7
Aatish Pandey <32216307+AatPa@users.noreply.github.com>
2021-09-10 11:30:56 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var username = prompt("Enter your name");
-alert("Hi! " + username + "Welcome to my site");
+var btnTranslate =  document.querySelector("#btn-translate");
+
+btnTranslate.addEventListener("click",clickHandle)
+
+var txtInput = document.querySelector("#txt-input");
+
+// console.log(txtInput);
+
+function clickHandle()
+{
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+}
+
